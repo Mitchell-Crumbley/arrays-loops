c583ec72fc84471ec6793883abf0b7bcb8db19f8
Mitchell Crumbley <mitchellcrumbley@gmail.com>
2021-01-19 20:38:49 -0600

    Array-methods

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log ("Setup for Class complete");
+const array1 = ['cow', 'dog', 'cat'];
+const string1 = array1.join(); //.join takes everything in an array an creates one string with it.
+const string1ToArray = string1.split(','); 
+// console.log(string1);
+// console.log(string1ToArray);
+
+// ****** .push() & .pop() *******
+// array1.pop();
+// console.log(array1);
+// array1.push('cat');
+// console.log(array1);
+
+// .unshift() & .shift() 
+array1.unshift('cow');
+console.log(array1);
+array1.shift();
+console.log(array1);
