2c5739f71b5b5dd40f34e637f1451827758c7e87
Mitchell Crumbley <mitchellcrumbley@gmail.com>
2021-01-19 19:43:40 -0600

    Array stuff

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log ("Setup for Class complete");
+const students = ['Jim', 'John', 'Matthew', 'Chie'];
+//console.log(students.length);
+
+// ************** ACCESSING ITEMS IN ARRAY ******
+
+//console.log(students[1]);
+//console.log(students[students.length -1]); //Chie. Console log the last name in the array. must have student.length within the brackets. 
+
+
+// Reassign a value within an array
+// students[0] = 'Jesse';
+// console.log(students);
+
+// const valuePrinter = (array, index) => {
+//   return array[index];
+// }
+
+// console.log(valuePrinter(students, 2)); //returns the 2 spot of the variable within the array defined as students. 
+
+
+//Write a function that takens an array of something and tells you if the name 'Greg' is in that array. 
+
+// const isGregHere = (array) => {
+//   return array.inclues('Greg');
+// };
+// console.log(isGregHere(students));
+
+// const students = ['Greg', 'Matthew', 'Jim'];
+// const isGregHere = () => {
+//     return students.inclues('Greg');
+//   };
+
+console.log(students.includes('Greg'));
